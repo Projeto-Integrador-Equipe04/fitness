cbd5157c2c15f0a8b9b408c52968152efade60fc
Lucas Manhães <ramosmanhaes@outlook.com>
2025-02-27 16:18:06 -0300

    Liberando rota para IMCController

--- src/main/java/com/generation/fitness/controller/IMCController.java
+++ src/main/java/com/generation/fitness/controller/IMCController.java
@@ -2,6 +2,7 @@ package com.generation.fitness.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.generation.fitness.service.IMCService;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class IMCController {
 
-	
 	@Autowired
     private IMCService imcService;
 
